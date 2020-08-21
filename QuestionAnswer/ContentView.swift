2cfa3200c46b64023bb787ffcb0a4fe88321d2d5
slavabily <slavabily@me.com>
2020-08-21 09:34:35 +0300

    Initial Commit

---
+++ QuestionAnswer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  QuestionAnswer
+//
+//  Created by slava bily on 21/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
