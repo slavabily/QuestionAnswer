0211654bb34381ccf4f922dc994567bcffe8451a
slavabily <slavabily@me.com>
2020-08-24 11:31:16 +0300

    the question making routing is added

--- QuestionAnswer/ContentView.swift
+++ QuestionAnswer/ContentView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var question = ""
+    @State private var answer = ""
+    
+    let words = ["I", "You", "He", "We"]
+    
+    let translations = ["Yo", "Tu", "el", "Nosotras"]
+    
+    let dict = [
+        "I": "Yo",
+        "You": "Tu",
+        "He": "el",
+        "We": "Nosotras"
+    ]
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            Text("Please, select correct translation:\n \(question)")
+                .font(.title)
+                .padding()
+            
+            HStack {
+                Button(" ") {
+                     
+                   
+                }
+                .font(.headline)
+                .padding()
+                
+                Spacer()
+                
+                Button(" ") {
+                     
+                    
+                }
+                .font(.headline)
+                .padding()
+            }
+            .offset(y: 100)
+            
+            Button("Continue") {
+                nextWord()
+            }
+            .font(.title)
+            
+        }
+        .onAppear {
+            nextWord()
+        }
+    }
+    
+    func nextWord() {
+        question = words.randomElement()!
+    }
+    
+    func translation() -> String {
+        translations.randomElement()!
+    }
+    
+    func isCorrect(question: String, answer: String) -> Bool {
+        
+        if dict[question] == answer {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
@@ -18,3 +82,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
